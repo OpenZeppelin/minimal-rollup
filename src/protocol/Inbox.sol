f930aebb180b88c1bcafe82952c65bcbec5af681
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-13 14:14:50 +1100

    Add NatSpec comments to Inbox.sol

--- src/protocol/Inbox.sol
+++ src/protocol/Inbox.sol
@@ -8,8 +8,10 @@ contract Inbox {
     // TODO: Optimize using the ring buffer design if we don't need to store all checkpoints
     // Checkpoints can be anything that describes the state of the rollup at a given publication (the most common case is
     // the state root)
+    /// @dev tracks proven checkpoints after applying the publication at `_dataFeed.getPublicationHash(pubIdx)`
     mapping(uint256 pubIdx => bytes32 checkpoint) checkpoints;
 
+    /// @dev the highest `pubIdx` in `checkpoints`
     uint256 lastProvenIdx;
 
     IDataFeed immutable _dataFeed;
@@ -17,6 +19,9 @@ contract Inbox {
     // just setting it in the constructor
     IVerifier immutable _verifier;
 
+    /// @param genesis the checkpoint describing the initial state of the rollup
+    /// @param dataFeed the input data source that updates the state of this rollup
+    /// @param verifier a contract that can verify the validity of a transition from one checkpoint to another
     constructor(bytes32 genesis, address dataFeed, address verifier) {
         // set the genesis checkpoint of the rollup - genesis is trusted to be correct
         checkpoints[0] = genesis;
@@ -24,8 +29,12 @@ contract Inbox {
         _verifier = IVerifier(verifier);
     }
 
-    ///  @notice Proves that the transition between the start and end publication hashes is valid
-    //          and updates the last proven index if checkpoint corresponds to a newer publication
+    /// @notice Proves the transition between two checkpoints
+    /// @dev Updates the `lastProvenIdx` to `end` on success
+    /// @param start the index of the publication before this transition. Its checkpoint must already be proven.
+    /// @param end the index of the last publication in this transition.
+    /// @param checkpoint the claimed checkpoint at the end of this transition.
+    /// @param proof arbitrary data passed to the `_verifier` contract to confirm the transition validity.
     function proveBetween(uint256 start, uint256 end, bytes32 checkpoint, bytes calldata proof) external {
         require(end > lastProvenIdx, "Publication already proven");
         bytes32 base = checkpoints[start];
