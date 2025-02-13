15df2d9f013c6af59cab7c723a089f775730576f
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-13 14:24:46 +1100

    Add NatSpec comments to DataFeed

--- src/protocol/DataFeed.sol
+++ src/protocol/DataFeed.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.28;
 
 contract DataFeed {
+
+    /// @dev a list of hashes identifying all data accompanying calls to the `publish` function.
     bytes32[] publicationHashes;
 
     constructor() {
@@ -9,6 +11,10 @@ contract DataFeed {
         publicationHashes.push(0);
     }
 
+    /// @notice Publish arbitrary data in blobs for data availability.
+    /// @param numBlobs the number of blobs accompanying this function call.
+    /// @dev append a hash representing all blobs and L1 contextual information to `publicationHashes`.
+    /// The number of blobs is not validated. Additional blobs are ignored. Empty blobs have a hash of zero.
     function publish(uint256 numBlobs) external {
         bytes32[] memory blobHashes = new bytes32[](numBlobs);
         for (uint256 i = 0; i < numBlobs; ++i) {
@@ -19,6 +25,9 @@ contract DataFeed {
         publicationHashes.push(pubHash);
     }
 
+    /// @notice retrieve a hash representing a previous publication
+    /// @param idx the index of the publication hash
+    /// @return _ the corresponding publication hash
     function getPublicationHash(uint256 idx) external view returns (bytes32) {
         return publicationHashes[idx];
     }
