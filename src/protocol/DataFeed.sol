c5f051d862d96ed3d26f478be2f6c08bbc7f5ff0
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-17 17:53:22 +0800

    Use function calls to retrieve L1 Metadata

--- src/protocol/DataFeed.sol
+++ src/protocol/DataFeed.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {IDataFeed} from "./IDataFeed.sol";
+import {IDataFeed, MetadataQuery} from "./IDataFeed.sol";
+import {IL1MetadataProvider} from "./IL1MetadataProvider.sol";
+
 
 contract DataFeed is IDataFeed {
     /// @dev a list of hashes identifying all data accompanying calls to the `publish` function.
@@ -14,15 +16,23 @@ contract DataFeed is IDataFeed {
 
     /// @notice Publish arbitrary data in blobs for data availability.
     /// @param numBlobs the number of blobs accompanying this function call.
-    /// @dev append a hash representing all blobs and L1 contextual information to `publicationHashes`.
+    /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    /// @dev append a hash representing all blobs and L1 metadata to `publicationHashes`.
     /// The number of blobs is not validated. Additional blobs are ignored. Empty blobs have a hash of zero.
-    function publish(uint256 numBlobs) external {
+    function publish(uint256 numBlobs, MetadataQuery[] calldata queries) external {
         bytes32[] memory blobHashes = new bytes32[](numBlobs);
         for (uint256 i = 0; i < numBlobs; ++i) {
             blobHashes[i] = blobhash(i);
         }
+
+        uint256 nQueries = queries.length;
+        bytes32[] memory metadataHashes = new bytes32[](nQueries);
+        for (uint256 i = 0; i < nQueries; ++i) {
+            metadataHashes[i] = IL1MetadataProvider(queries[i].provider).getL1MetadataHash(queries[i].input);
+        } 
+
         bytes32 prevHash = publicationHashes[publicationHashes.length - 1];
-        bytes32 pubHash = keccak256(abi.encode(prevHash, msg.sender, block.timestamp, blobHashes));
+        bytes32 pubHash = keccak256(abi.encode(prevHash, blobHashes, metadataHashes));
         publicationHashes.push(pubHash);
 
         emit Publication(pubHash);
--- src/protocol/IDataFeed.sol
+++ src/protocol/IDataFeed.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+struct MetadataQuery {
+    address provider;
+    bytes input;
+}
+
 interface IDataFeed {
     /// @notice Emitted when a new publication is created
     /// @param pubHash the hash of the new publication
     event Publication(bytes32 indexed pubHash);
 
     /// @notice Publish data as blobs for data availability
-    function publish(uint256 numBlobs) external;
+    /// @param numBlobs the number of blobs accompanying this function call.
+    /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    function publish(uint256 numBlobs, MetadataQuery[] calldata queries) external;
 
     /// @notice Returns the hash of the publication at the given index
     function getPublicationHash(uint256 idx) external view returns (bytes32);
