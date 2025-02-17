dc408fe12ca332a0a307b50f3f16fb1fe232b105
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-17 17:58:24 +0800

    Explain why queries is an array

--- src/protocol/DataFeed.sol
+++ src/protocol/DataFeed.sol
@@ -17,6 +17,8 @@ contract DataFeed is IDataFeed {
     /// @notice Publish arbitrary data in blobs for data availability.
     /// @param numBlobs the number of blobs accompanying this function call.
     /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    /// @dev there can be multiple queries because a single publication might represent multiple rollups,
+    /// each with their own L1 metadata requirements
     /// @dev append a hash representing all blobs and L1 metadata to `publicationHashes`.
     /// The number of blobs is not validated. Additional blobs are ignored. Empty blobs have a hash of zero.
     function publish(uint256 numBlobs, MetadataQuery[] calldata queries) external {
