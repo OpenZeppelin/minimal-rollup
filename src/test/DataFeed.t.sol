04966cd717ff1d200bc3345cbd3295afc546f3f7
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-18 12:50:21 +0800

    Create trivial tests
    This is just to silence the CI error.
    We should create more comprehensive tests when we have a skeleton solution

---
+++ src/test/DataFeed.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {DataFeed} from "src/protocol/DataFeed.sol";
+import {IDataFeed} from "src/protocol/IDataFeed.sol";
+
+contract DataFeedTest is Test {
+    DataFeed feed;
+
+    function setUp() public {
+        feed = new DataFeed();
+    }
+
+    function testRevert_NoBlobs() public {
+        vm.expectRevert();
+        IDataFeed.MetadataQuery[] memory queries = new IDataFeed.MetadataQuery[](0);
+        feed.publish(0, queries);
+    }
+
+    function test_EmptyBlobDoesNotRevert() public {
+        IDataFeed.MetadataQuery[] memory queries = new IDataFeed.MetadataQuery[](0);
+        feed.publish(1, queries);
+    }
+}
