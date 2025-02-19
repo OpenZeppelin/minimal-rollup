8356892a4fefe5e06c860cb52cf2350ca7f8a4dc
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-19 11:29:49 +0800

    Use @inheritdoc in DataFeed

--- src/protocol/IDataFeed.sol
+++ src/protocol/IDataFeed.sol
@@ -42,19 +42,30 @@ interface IDataFeed {
     /// @param publication the DirectPublication struct describing the preimages to pubHash
     event DirectPublished(bytes32 indexed pubHash, DirectPublication publication);
 
-    /// @notice Publish data as blobs for data availability
+    /// @notice Publish arbitrary data in blobs for data availability.
     /// @param numBlobs the number of blobs accompanying this function call.
     /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    /// @dev there can be multiple queries because a single publication might represent multiple rollups,
+    /// each with their own L1 metadata requirements
+    /// @dev append a hash representing all blobs and L1 metadata to `publicationHashes`.
+    /// The number of blobs is not validated. Additional blobs are ignored. Empty blobs have a hash of zero.
     function publish(uint256 numBlobs, MetadataQuery[] calldata queries) external payable;
 
     /// @notice Publish arbitrary data for data availability.
     /// @param data the data to publish
     /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    /// @dev there can be multiple queries because a single publication might represent multiple rollups,
+    /// each with their own L1 metadata requirements
+    /// @dev append a hash representing the data and L1 metadata to `directPublicationHashes`.
     function directPublish(bytes data, MetadataQuery[] calldata queries) external payable;
 
-    /// @notice Returns the hash of the publication at the given index
+    /// @notice retrieve a hash representing a previous publication
+    /// @param idx the index of the publication hash
+    /// @return _ the corresponding publication hash
     function getPublicationHash(uint256 idx) external view returns (bytes32);
 
     /// @notice retrieve a hash representing a previous direct publication
+    /// @param idx the index of the direct publication hash
+    /// @return _ the corresponding direct publication hash
     function getDirectPublicationHash(uint256 idx) external view returns (bytes32);
 }
