3c32c60398620a6d9617593fa6efb9a092d85c6b
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-19 11:14:05 +0800

    Duplicate publication functionality
    There is now an independent queue using calldata instead of blobs.
    They need to be combined.
    They should also be refactored for code sharing

--- src/protocol/DataFeed.sol
+++ src/protocol/DataFeed.sol
@@ -15,6 +15,9 @@ contract DataFeed is IDataFeed {
     /// @dev a list of hashes identifying all data accompanying calls to the `publish` function.
     bytes32[] public publicationHashes;
 
+    /// @dev a list of hashes identifying all data passed to the `directPublish` function.
+    bytes32[] public directPublicationHashes;
+
     modifier onlyStandaloneTx() {
         require(!_TRANSACTION_GUARD.asBoolean().tload());
         _TRANSACTION_GUARD.asBoolean().tstore(true);
@@ -25,6 +28,7 @@ contract DataFeed is IDataFeed {
     constructor() {
         // guarantee there is always a previous hash
         publicationHashes.push(0);
+        directPublicationHashes.push(0);
     }
 
     /// @notice Publish arbitrary data in blobs for data availability.
@@ -69,10 +73,54 @@ contract DataFeed is IDataFeed {
         emit Published(pubHash, publication);
     }
 
+    /// @notice Publish arbitrary data for data availability.
+    /// @param data the data to publish
+    /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    /// @dev there can be multiple queries because a single publication might represent multiple rollups,
+    /// each with their own L1 metadata requirements
+    /// @dev append a hash representing the data and L1 metadata to `directPublicationHashes`.
+    function directPublish(bytes data, MetadataQuery[] calldata queries) external payable {
+        require(data.length > 0, "no data to publish");
+
+        uint256 nQueries = queries.length;
+        uint256 id = directPublicationHashes.length;
+        DirectPublication memory publication = DirectPublication({
+            id: id,
+            prevHash: directPublicationHashes[id - 1],
+            publisher: msg.sender,
+            timestamp: block.timestamp,
+            blockNumber: block.number,
+            dataHash: keccak256(data),
+            queries: queries,
+            metadata: new bytes[](nQueries)
+        });
+
+        uint256 totalValue;
+        for (uint256 i; i < nQueries; ++i) {
+            publication.metadata[i] = IMetadataProvider(queries[i].provider).getMetadata{value: queries[i].value}(
+                msg.sender, queries[i].input
+            );
+            totalValue += queries[i].value;
+        }
+        require(msg.value == totalValue, "Incorrect ETH passed with publication");
+
+        bytes32 pubHash = keccak256(abi.encode(publication));
+        directPublicationHashes.push(pubHash);
+
+        emit DirectPublished(pubHash, publication)
+    }
+
     /// @notice retrieve a hash representing a previous publication
     /// @param idx the index of the publication hash
     /// @return _ the corresponding publication hash
     function getPublicationHash(uint256 idx) external view returns (bytes32) {
         return publicationHashes[idx];
+    }
+
+    /// @notice retrieve a hash representing a previous direct publication
+    /// @param idx the index of the direct publication hash
+    /// @return _ the corresponding direct publication hash
+    function getDirectPublicationHash(uint256 idx) external view returns (bytes32) {
+        return directPublicationHashes[idx];
     }
 }
--- src/protocol/IDataFeed.sol
+++ src/protocol/IDataFeed.sol
@@ -19,16 +19,40 @@ interface IDataFeed {
         bytes[] metadata;
     }
 
+    struct DirectPublication {
+        uint256 id;
+        bytes32 prevHash;
+        address publisher;
+        uint256 timestamp;
+        uint256 blockNumber;
+        bytes32 dataHash;
+        MetadataQuery[] queries;
+        bytes[] metadata;
+    }
+
     /// @notice Emitted when a new publication is created
     /// @param pubHash the hash of the new publication
     /// @param publication the Publication struct describing the preimages to pubHash
     event Published(bytes32 indexed pubHash, Publication publication);
+
+    /// @notice Emitted when a new direct publication is created
+    /// @param pubHash the hash of the new publication
+    /// @param publication the DirectPublication struct describing the preimages to pubHash
+    event DirectPublished(bytes32 indexed pubHash, DirectPublication publication);
 
     /// @notice Publish data as blobs for data availability
     /// @param numBlobs the number of blobs accompanying this function call.
     /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
     function publish(uint256 numBlobs, MetadataQuery[] calldata queries) external payable;
 
+    /// @notice Publish arbitrary data for data availability.
+    /// @param data the data to publish
+    /// @param queries the calls required to retrieve L1 metadata hashes associated with this publication.
+    function directPublish(bytes data, MetadataQuery[] calldata queries) external payable;
+
     /// @notice Returns the hash of the publication at the given index
     function getPublicationHash(uint256 idx) external view returns (bytes32);
+
+    /// @notice retrieve a hash representing a previous direct publication
+    function getDirectPublicationHash(uint256 idx) external view returns (bytes32);
 }
