5b42d0fe8cc6da49ea77d671314471a94673a9a1
Daniel Wang <dan@taikocha.in>
2025-02-20 21:27:52 +0800

    Update ITaikoData.sol

--- src/protocol/taiko_alethia/ITaikoData.sol
+++ src/protocol/taiko_alethia/ITaikoData.sol
@@ -2,10 +2,18 @@
 pragma solidity ^0.8.28;
 
 interface ITaikoData {
+    /// @dev Represents a data source for a proposal, containing multiple blobs.
+    /// These blobs are concatenated into a single blob or string, which then is decoded into a specific format as
+    /// defined by the node or client.
     struct DataSource {
         bytes32[] blobs;
     }
 
+    /// @dev Represents a proposal, which includes an anchor block hash and an array of data sources.
+    /// The anchor block hash may be set to 0, indicating that the anchor transaction should use 0 as the anchor block
+    /// identifier.
+    /// Each data source is decoded into a data object, and these objects are concatenated into a list. This list is
+    /// then used to derive Taiko blocks.
     struct Proposal {
         bytes32 anchorBlockhash;
         DataSource[] proposalDataList;
