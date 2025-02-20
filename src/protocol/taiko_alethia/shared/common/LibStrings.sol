cc99d4b8d6a1ea9de9e1f40b1b958d7daf852178
LeoPatOZ <leonard.paturel@openzeppelin.com>
2025-02-20 12:21:04 +0800

    add erc7786
    rename

--- src/protocol/taiko_alethia/shared/common/LibStrings.sol
+++ src/protocol/taiko_alethia/shared/common/LibStrings.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 /*
-Note: This is taken from Taiko's Alethia repo
+@dev This is taken from Taiko's Alethia repo
 (https://github.com/taikoxyz/taiko-mono/blob/taiko-alethia-client-v0.43.1/packages/protocol/contracts/shared/common/LibStrings.sol) 
     And does not include the full implementation of the contract. Instead only the signal service constant is included.
 */
