ff532c55473081344a9d192771002257a3e5a609
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-21 15:44:23 +0800

    Simply data feed to remove hooks
    This is currently inconsistent with the rest of the code base.
    We will need to restore the functionality to the inbox.

--- src/protocol/DataFeed.sol
+++ src/protocol/DataFeed.sol
@@ -14,22 +14,7 @@ contract DataFeed is IDataFeed {
     }
 
     /// @inheritdoc IDataFeed
-    function publish(
-        uint256 numBlobs,
-        bytes calldata data,
-        HookQuery[] calldata preHookQueries,
-        HookQuery[] calldata postHookQueries
-    ) external payable {
-        uint256 nHooks = preHookQueries.length;
-        uint256 totalValue;
-        bytes[] memory auxData = new bytes[](nHooks);
-        for (uint256 i; i < nHooks; ++i) {
-            auxData[i] = IPublicationHook(preHookQueries[i].provider).beforePublish{value: preHookQueries[i].value}(
-                msg.sender, preHookQueries[i].input
-            );
-            totalValue += preHookQueries[i].value;
-        }
-
+    function publish(bytes calldata data) external {
         uint256 id = publicationHashes.length;
         Publication memory publication = Publication({
             id: id,
@@ -37,28 +22,11 @@ contract DataFeed is IDataFeed {
             publisher: msg.sender,
             timestamp: block.timestamp,
             blockNumber: block.number,
-            blobHashes: new bytes32[](numBlobs),
-            data: data,
-            preHookQueries: preHookQueries,
-            postHookQueries: postHookQueries,
-            auxData: auxData
+            data: data
         });
-
-        for (uint256 i; i < numBlobs; ++i) {
-            publication.blobHashes[i] = blobhash(i);
-        }
 
         bytes32 pubHash = keccak256(abi.encode(publication));
         publicationHashes.push(pubHash);
-
-        nHooks = postHookQueries.length;
-        for (uint256 i; i < nHooks; ++i) {
-            IPublicationHook(postHookQueries[i].provider).afterPublish{value: postHookQueries[i].value}(
-                msg.sender, publication, postHookQueries[i].input
-            );
-            totalValue += postHookQueries[i].value;
-        }
-        require(msg.value == totalValue, "Incorrect ETH passed with publication");
 
         emit Published(pubHash, publication);
     }
