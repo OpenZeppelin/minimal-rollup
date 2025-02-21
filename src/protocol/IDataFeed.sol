9a72c590f43ed0374f72439ff9df8884f00a8127
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-21 17:22:22 +0800

    Rename Publication to PublicationHeader
    It no longer references the data field

--- src/protocol/IDataFeed.sol
+++ src/protocol/IDataFeed.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 interface IDataFeed {
-    struct Publication {
+    struct PublicationHeader {
         uint256 id;
         bytes32 prevHash;
         address publisher;
@@ -11,14 +11,16 @@ interface IDataFeed {
     }
 
     /// @notice Emitted when a new publication is created
-    /// @param pubHash the hash of the new publication and attributes
-    /// @param publication the Publication struct describing the publication metadata
-    /// @param attributes the data associated with the publication
-    event Published(bytes32 indexed pubHash, Publication publication, bytes[] attributes);
+    /// @param pubHash the hash of the new publication
+    /// @param header the metadata associated with the publication
+    /// @param attributes the data contained within the publication
+    event Published(bytes32 indexed pubHash, PublicationHeader header, bytes[] attributes);
 
     /// @notice Publish arbitrary data into the global queue.
-    /// @param data the data to publish.
-    function publish(bytes[] calldata data) external;
+    /// @param attributes the data to publish.
+    /// @dev the data is encoded as an array so each attribute is hashed independently,
+    /// which allows a single attribute to be validated against a pubHash
+    function publish(bytes[] calldata attributes) external;
 
     /// @notice retrieve a hash representing a previous publication
     /// @param idx the index of the publication hash
