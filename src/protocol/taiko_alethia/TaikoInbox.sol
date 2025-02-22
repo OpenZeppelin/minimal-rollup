a0475e3c167b08f847e508167f642c9a9d9cf587
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-02-22 15:54:31 +0800

    Use IBlobRefRegistry in TaikoInbox (#34)
    Co-authored-by: Ernesto García <ernestognw@gmail.com>

--- src/protocol/taiko_alethia/TaikoInbox.sol
+++ src/protocol/taiko_alethia/TaikoInbox.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {IBlobRefRegistry} from "../../blobs/IBlobRefRegistry.sol";
+
 import {IDataFeed} from "../IDataFeed.sol";
 
 import {IDelayedInclusionStore} from "./IDelayedInclusionStore.sol";
 import {ILookahead} from "./ILookahead.sol";
-import {ITaikoData} from "./ITaikoData.sol";
 
 contract TaikoInbox {
     IDataFeed public immutable datafeed;
     ILookahead public immutable lookahead;
+    IBlobRefRegistry public immutable blobRefRegistry;
     IDelayedInclusionStore public immutable delayedInclusionStore;
 
     uint256 public immutable maxAnchorBlockIdOffset;
@@ -19,11 +21,13 @@ contract TaikoInbox {
     constructor(
         address _datafeed,
         address _lookahead,
+        address _blobRefRegistry,
         address _delayedInclusionStore,
         uint256 _maxAnchorBlockIdOffset
     ) {
         datafeed = IDataFeed(_datafeed);
         lookahead = ILookahead(_lookahead);
+        blobRefRegistry = IBlobRefRegistry(_blobRefRegistry);
         delayedInclusionStore = IDelayedInclusionStore(_delayedInclusionStore);
         maxAnchorBlockIdOffset = _maxAnchorBlockIdOffset;
     }
@@ -45,26 +49,27 @@ contract TaikoInbox {
         // Build the attribute to link back to the previous publication Id;
         attributes[1] = abi.encode(_prevPublicationId);
 
-        // Build the attribute for this proposal's data availability
-        bytes32[] memory blobHashes = new bytes32[](nBlobs);
-        for (uint256 i; i < nBlobs; ++i) {
-            blobHashes[i] = blobhash(i);
-            require(blobHashes[i] != 0, "data unavailable");
-        }
-        attributes[2] = abi.encode(blobHashes);
+        attributes[2] = abi.encode(blobRefRegistry.getRef(_buildBlobIndices(nBlobs)));
         _prevPublicationId = datafeed.publish(attributes).id;
 
         // Publish each inclusion as a publication
-        ITaikoData.DataSource[] memory dataSources =
+        IBlobRefRegistry.BlobRef[] memory blobRefs =
             delayedInclusionStore.processDelayedInclusionByDeadline(block.timestamp);
 
-        uint256 nDataSources = dataSources.length;
-        for (uint256 i; i < nDataSources; ++i) {
+        uint256 nBlobRefs = blobRefs.length;
+        for (uint256 i; i < nBlobRefs; ++i) {
             attributes[1] = abi.encode(_prevPublicationId);
-            attributes[2] = abi.encode(dataSources[i].blobHashes);
+            attributes[2] = abi.encode(blobRefs[i]);
             _prevPublicationId = datafeed.publish(attributes).id;
         }
 
         prevPublicationId = _prevPublicationId;
+    }
+
+    function _buildBlobIndices(uint256 nBlobs) private pure returns (uint256[] memory blobIndices) {
+        blobIndices = new uint256[](nBlobs);
+        for (uint256 i; i < nBlobs; ++i) {
+            blobIndices[i] = i;
+        }
     }
 }
