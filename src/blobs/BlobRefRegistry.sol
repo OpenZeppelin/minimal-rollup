3b882c86b0217ee4f5d1a9a22cd9f56f24ff2406
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-02-22 15:11:51 +0800

    Introduce BlobRegistry (#30)
    Co-authored-by: Nikesh Nazareth <business@nikeshnazareth.com>
    Co-authored-by: Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
    Co-authored-by: Ernesto García <ernestognw@gmail.com>

---
+++ src/blobs/BlobRefRegistry.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./IBlobRefRegistry.sol";
+
+/// @title BlobRefRegistry
+contract BlobRefRegistry is IBlobRefRegistry {
+    /// @dev A mapping of the hash of a blob ref to the timestamp when it was saved
+    mapping(bytes32 refHash => uint256 timestamp) private _savedRefHashes;
+
+    /// @inheritdoc IBlobRefRegistry
+    function getRefAndSaveHash(uint256[] calldata blobIndices) external returns (BlobRef memory ref, bytes32 refHash) {
+        ref = _getRef(blobIndices);
+        refHash = _saveRefHash(ref);
+    }
+
+    /// @inheritdoc IBlobRefRegistry
+    function getRef(uint256[] calldata blobIndices) external view returns (BlobRef memory) {
+        return _getRef(blobIndices);
+    }
+
+    /// @inheritdoc IBlobRefRegistry
+    function isRefKnown(BlobRef memory ref) external view returns (bool) {
+        return _savedRefHashes[keccak256(abi.encode(ref))] != 0;
+    }
+
+    /// @dev Saves the hash of a blob ref to the registry
+    /// @param ref The blob ref whose hash to save
+    /// @return The hash of the blob source
+    function _saveRefHash(BlobRef memory ref) private returns (bytes32) {
+        bytes32 hash = keccak256(abi.encode(ref));
+        _savedRefHashes[hash] = block.timestamp;
+        return hash;
+    }
+
+    /// @dev Retrieves the blob ref for given blob indices
+    /// @param blobIndices The indices of the blobhashes to retrieve
+    /// @return The blob ref constructed from the block's number and the list of blob hashes
+    function _getRef(uint256[] calldata blobIndices) private view returns (BlobRef memory) {
+        uint256 nBlobs = blobIndices.length;
+        require(nBlobs != 0, "No blobs provided");
+
+        bytes32[] memory blobhashes = new bytes32[](nBlobs);
+        for (uint256 i; i < nBlobs; ++i) {
+            blobhashes[i] = blobhash(blobIndices[i]);
+            require(blobhashes[i] != 0, "Blob not found");
+        }
+
+        return BlobRef(block.number, blobhashes);
+    }
+}
---
+++ src/blobs/IBlobRefRegistry.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/// @title IBlobRefRegistry
+/// @notice Interface for accessing and registering blob hashes from transactions
+interface IBlobRefRegistry {
+    /// @dev Struct containing blob hashes from a specific block
+    /// @param blockNumber The block number where the blobs were included
+    /// @param blobs Array of blob hashes
+    struct BlobRef {
+        uint256 blockNumber;
+        bytes32[] blobhashes;
+    }
+
+    /// @notice Validates blobs at given indices, return a ref object, then save the ref hash for later usage
+    /// @param blobIndices Array of blob indices to retrieve
+    /// @return ref The retrieved blob data including block number and blob hashes
+    /// @return refHash The keccak256 hash of the encoded blob ref
+    /// @dev Should revert if any blob index is invalid or if no blobs are provided
+    function getRefAndSaveHash(uint256[] calldata blobIndices) external returns (BlobRef memory ref, bytes32 refHash);
+
+    /// @notice Validates blobs at given indices and return a ref object
+    /// @param blobIndices Array of blob indices to retrieve
+    /// @return The blob data including block number and blob hashes
+    /// @dev Should revert if any blob index is invalid or if no blobs are provided
+    function getRef(uint256[] calldata blobIndices) external view returns (BlobRef memory);
+
+    /// @notice Checks if a blob reference has been previously saved
+    /// @param ref The blob ref to check
+    /// @return True if the blob ref hash exists in the registry, false otherwise
+    function isRefKnown(BlobRef memory ref) external view returns (bool);
+}
