0127e34137e3aa7da88efa5e458bdd8a59ce31b3
Daniel Wang <dan@taikocha.in>
2025-02-22 21:30:08 +0800

    Implement Signal Service using Merkle Mountain Ranges (MMR)

---
+++ src/protocol/ISignalService.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+///@title ISignalService
+///@notice Interface for sending signals and verifying signals for cross-chain communication.
+interface ISignalService {
+    ///@notice Emitted for each signal
+    ///@param signal The signal value
+    ///@param count The number of signals after the signal was sent
+    ///@param blockNumber The block number of the signal
+    event Signal(bytes32 indexed signal, uint256 indexed count, uint256 indexed blockNumber);
+
+    /// @notice Sends a signal and update MMR peaks
+    /// @param signal The signal value
+    ///@dev This function should be called on the source chain
+    function send(bytes32 signal) external;
+
+    /// @notice Verifies a signal has been sent using a merkle proof
+    /// @param count The number of signals after the signal was sent
+    /// @param value The signal value
+    /// @param proof The merkle proof of the signal
+    ///@dev This function should be called on the destination chain
+    function verify(uint256 count, bytes32 value, bytes32[] memory proof) external view;
+}
---
+++ src/protocol/SignalService.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ISignalService} from "./ISignalService.sol";
+import {StatelessMmr} from "solidity-mmr/lib/StatelessMmr.sol";
+
+///@title SignalService
+///@notice An implementation of the ISignalService interface using Merkle Mountain Ranges (MMR).
+contract SignalService is ISignalService {
+    uint256 private _count;
+    bytes32 private _root;
+    bytes32[] private _peaks;
+
+    ///@inheritdoc ISignalService
+    function send(bytes32 signal) external {
+        (uint256 count, bytes32 root, bytes32[] memory peaks) =
+            StatelessMmr.appendWithPeaksRetrieval(signal, _peaks, _count, _root);
+
+        _peaks = peaks;
+        _count = count;
+        _root = root;
+        emit Signal(signal, count, block.number);
+    }
+
+    /// @inheritdoc ISignalService
+    function verify(uint256 count, bytes32 value, bytes32[] memory proof) external view {
+        StatelessMmr.verifyProof(count, value, proof, _peaks, _count, _root);
+    }
+
+    /// @notice Returns the current MMR root
+    /// @return The current MMR root
+    function getRoot() external view returns (bytes32) {
+        return _root;
+    }
+
+    /// @notice Returns the current MMR peaks
+    /// @return The current MMR peaks
+    function getPeaks() external view returns (bytes32[] memory) {
+        return _peaks;
+    }
+
+    /// @notice Returns the current number of signals
+    /// @return The current number of signals
+    function getCount() external view returns (uint256) {
+        return _count;
+    }
+}
