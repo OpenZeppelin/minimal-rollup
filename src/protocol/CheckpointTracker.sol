31028e28fdf006c14265c2887c838b4e5b56d193
Nikesh Nazareth <business@nikeshnazareth.com>
2025-02-23 20:06:25 +0800

    Remove underscores in CheckpointTracker immutable names
    This is consistent with the rest of the code base

--- src/protocol/CheckpointTracker.sol
+++ src/protocol/CheckpointTracker.sol
@@ -10,18 +10,17 @@ struct Checkpoint {
 }
 
 contract CheckpointTracker {
-
     /// @notice The current proven checkpoint representing the latest verified state of the rollup
     /// @dev Previous checkpoints are not stored here but are synchronized to the `SignalService`
     /// @dev A checkpoint commitment is any value (typically a state root) that uniquely identifies
     /// the state of the rollup at a specific point in time
     Checkpoint public checkpoint;
 
-    IPublicationFeed public immutable _publicationFeed;
+    IPublicationFeed public immutable publicationFeed;
 
     // This would usually be retrieved dynamically as in the current Taiko implementation, but for simplicity we are
     // just setting it in the constructor
-    IVerifier public immutable _verifier;
+    IVerifier public immutable verifier;
 
     /// @notice Emitted when a checkpoint is proven
     /// @param publicationId the index of the publication at which the commitment was proven
@@ -29,14 +28,14 @@ contract CheckpointTracker {
     event CheckpointProven(uint256 indexed publicationId, bytes32 indexed commitment);
 
     /// @param genesis the checkpoint commitment describing the initial state of the rollup
-    /// @param publicationFeed the input data source that updates the state of this rollup
-    /// @param verifier a contract that can verify the validity of a transition from one checkpoint to another
-    constructor(bytes32 genesis, address publicationFeed, address verifier) {
+    /// @param _publicationFeed the input data source that updates the state of this rollup
+    /// @param _verifier a contract that can verify the validity of a transition from one checkpoint to another
+    constructor(bytes32 genesis, address _publicationFeed, address _verifier) {
         // set the genesis checkpoint commitment of the rollup - genesis is trusted to be correct
         require(genesis != 0, "genesis checkpoint commitment cannot be 0");
         checkpoint.commitment = genesis;
-        _publicationFeed = IPublicationFeed(publicationFeed);
-        _verifier = IVerifier(verifier);
+        publicationFeed = IPublicationFeed(_publicationFeed);
+        verifier = IVerifier(_verifier);
     }
 
     /// @notice Verifies and updates the rollup state with a new checkpoint
@@ -46,9 +45,9 @@ contract CheckpointTracker {
         require(newCheckpoint.commitment != 0, "Commitment cannot be 0");
         require(newCheckpoint.publicationId > checkpoint.publicationId, "Publication already proven");
 
-        IVerifier(_verifier).verifyProof(
-            _publicationFeed.getPublicationHash(checkpoint.publicationId),
-            _publicationFeed.getPublicationHash(newCheckpoint.publicationId),
+        verifier.verifyProof(
+            publicationFeed.getPublicationHash(checkpoint.publicationId),
+            publicationFeed.getPublicationHash(newCheckpoint.publicationId),
             checkpoint.commitment,
             newCheckpoint.commitment,
             proof
