633d43fba6ddd22206307ffc3a62ca38c4d6d701
Ernesto García <ernestognw@gmail.com>
2025-02-25 02:15:41 +0800

    [DRAFT] Add StateSyncer

---
+++ src/signal/state/IStateSyncer.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ISignalService} from "../ISignalService.sol";
+
+interface IStateSyncer {
+    event ChainDataSynced(uint64 indexed chainId, uint64 indexed blockNumber, bytes32 root);
+
+    function signalService() external view returns (ISignalService);
+
+    function syncSignal(uint64 chainId, uint64 blockNumber, bytes32 root) external view returns (bytes32 signal);
+
+    function stateSynced(uint64 chainId, uint64 blockNumber, bytes32 root) external view returns (bool synced);
+
+    function stateAt(uint64 chainId, uint64 blockNumber) external view returns (bytes32 root);
+
+    function latestState(uint64 chainId) external view returns (bytes32 root);
+
+    function latestBlock(uint64 chainId) external view returns (uint64 blockNumber);
+
+    function verifyState(uint64 chainId, uint64 blockNumber, bytes32 root, bytes[] calldata proof)
+        external
+        view
+        returns (bool valid);
+
+    function syncState(uint64 chainId, uint64 blockNumber, bytes32 root) external;
+}
---
+++ src/signal/state/StateSyncer.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {LibTrieProof} from "../../libs/LibTrieProof.sol";
+import {ISignalService} from "../ISignalService.sol";
+import {IStateSyncer} from "./IStateSyncer.sol";
+
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+abstract contract StateSyncer is IStateSyncer {
+    using LibTrieProof for bytes32;
+    using SafeCast for uint256;
+
+    address private immutable _signalService;
+
+    mapping(uint64 chainId => uint64 blockNumber) private _latestBlock;
+    mapping(uint64 chainId => mapping(uint64 blockNumber => bytes32 root)) private _stateAt;
+
+    modifier onlySyncer() {
+        _checkSyncer(msg.sender);
+        _;
+    }
+
+    constructor(address signalService_) {
+        _signalService = signalService_;
+    }
+
+    function syncSignal(uint64 chainId, uint64 blockNumber, bytes32 root)
+        public
+        view
+        virtual
+        returns (bytes32 signal)
+    {
+        return keccak256(abi.encode(chainId, blockNumber, root));
+    }
+
+    function signalService() public view virtual returns (ISignalService) {
+        return ISignalService(_signalService);
+    }
+
+    function stateSynced(uint64 chainId, uint64 blockNumber, bytes32 root) public view virtual returns (bool) {
+        return stateAt(chainId, blockNumber) == root;
+    }
+
+    function stateAt(uint64 chainId, uint64 blockNumber) public view virtual returns (bytes32 root) {
+        return _stateAt[chainId][blockNumber];
+    }
+
+    function latestState(uint64 chainId) public view virtual returns (bytes32 root) {
+        return stateAt(chainId, latestBlock(chainId));
+    }
+
+    function latestBlock(uint64 chainId) public view virtual returns (uint64 blockNumber) {
+        return _latestBlock[chainId];
+    }
+
+    function verifyState(uint64 chainId, uint64 blockNumber, bytes32 root, bytes[] calldata proof)
+        public
+        view
+        returns (bool valid)
+    {
+        bytes32 slot =
+            signalService().signalSlot(block.chainid.toUint64(), address(this), syncSignal(chainId, blockNumber, root));
+        return slot.verifyState(syncSignal(chainId, blockNumber, root), root, proof);
+    }
+
+    function syncState(uint64 chainId, uint64 blockNumber, bytes32 root) external virtual onlySyncer {
+        _syncState(chainId, blockNumber, root);
+    }
+
+    function _syncState(uint64 chainId, uint64 blockNumber, bytes32 root) internal virtual {
+        if (latestBlock(chainId) < blockNumber) {
+            _latestBlock[chainId] = blockNumber;
+            _stateAt[chainId][blockNumber] = root;
+            signalService().sendSignal(syncSignal(chainId, blockNumber, root));
+            emit ChainDataSynced(chainId, blockNumber, root);
+        }
+    }
+
+    /// @dev Must revert if the caller is not an authorized syncer.
+    function _checkSyncer(address caller) internal virtual;
+}
