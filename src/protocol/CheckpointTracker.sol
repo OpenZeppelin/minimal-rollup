c23367e6788e80b904fb8b494dfdc6048a12fa18
Daniel Wang <dan@taiko.xyz>
2025-02-25 14:25:18 +0800

    the change

--- src/protocol/CheckpointTracker.sol
+++ src/protocol/CheckpointTracker.sol
@@ -10,11 +10,12 @@ contract CheckpointTracker is ICheckpointTracker {
     /// @dev Previous checkpoints are not stored here but are synchronized to the `SignalService`
     /// @dev A checkpoint commitment is any value (typically a state root) that uniquely identifies
     /// the state of the rollup at a specific point in time
-    bytes32 public provenHash;
+    Checkpoint public latestCheckpoint;
+    bytes32 public latestCheckpointHash;
 
     /// @notice Verified transitions between two checkpoints
     /// @dev the start checkpoint is not necessarily valid, but the end checkpoint is correctly built on top of it.
-    mapping(bytes32 startHash => bytes32 endHash) public transitions;
+    mapping(bytes32 startCheckpointHash => Checkpoint endCheckpoint) public transitions;
 
     IPublicationFeed public immutable publicationFeed;
 
@@ -26,69 +27,71 @@ contract CheckpointTracker is ICheckpointTracker {
     /// @dev This limits the overhead required to submit a proof
     uint256 private constant MAX_EXTRA_UPDATES = 10; // TODO: What is a reasonable number here?
 
-    /// @param _genesis the checkpoint commitment describing the initial state of the rollup
+    /// @param _genesisComittment the commitment describing the initial state of the rollup
     /// @param _publicationFeed the input data source that updates the state of this rollup
     /// @param _verifier a contract that can verify the validity of a transition from one checkpoint to another
-    constructor(bytes32 _genesis, address _publicationFeed, address _verifier) {
-        // set the genesis checkpoint commitment of the rollup - genesis is trusted to be correct
-        require(_genesis != 0, "genesis checkpoint commitment cannot be 0");
-
+    constructor(bytes memory _genesisComittment, address _publicationFeed, address _verifier) {
         publicationFeed = IPublicationFeed(_publicationFeed);
         verifier = IVerifier(_verifier);
 
-        Checkpoint memory genesisCheckpoint = Checkpoint({publicationId: 0, commitment: _genesis});
-        provenHash = keccak256(abi.encode(genesisCheckpoint));
-        emit CheckpointUpdated(provenHash);
+        uint256 genesisPublicationId = 0;
+        bytes32 genesisCommitmentHash = keccak256(_genesisComittment);
+        latestCheckpointHash = keccak256(abi.encodePacked(genesisPublicationId, genesisCommitmentHash));
+
+        Checkpoint memory genesisCheckpoint = Checkpoint(genesisPublicationId, _genesisComittment);
+        latestCheckpoint = genesisCheckpoint;
+
+        emit CheckpointUpdated(genesisCommitmentHash, genesisCheckpoint);
     }
 
-    function proveTransition(Checkpoint calldata start, Checkpoint calldata end, bytes calldata proof) external {
-        require(end.commitment != 0, "Checkpoint commitment cannot be 0");
-        bytes32 startHash = keccak256(abi.encode(start));
+    function proveTransition(
+        uint256 startPublicationId,
+        uint256 startCommitmentHash,
+        Checkpoint calldata endCheckpoint,
+        bytes calldata proof
+    ) external {
+        require(endCheckpoint.commitment.length != 0, "Checkpoint commitment cannot be empty");
+        bytes32 startChekpointHash = keccak256(abi.encodePacked(startPublicationId, startCommitmentHash));
 
         // TODO: once the proving incentive mechanism is in place we should reconsider this requirement because
         // ideally we would use the proof that creates the longest chain of proven publications.
-        require(transitions[startHash] == 0, "Checkpoint already has valid transition");
-        require(start.publicationId < end.publicationId, "Start must be before end");
+        require(transitions[startChekpointHash].publicationId == 0, "Checkpoint already has valid transition");
+        require(startPublicationId < endCheckpoint.publicationId, "Start must be before end");
 
-        bytes32 endPublicationHash = publicationFeed.getPublicationHash(end.publicationId);
+        bytes32 startPublicationHash = publicationFeed.getPublicationHash(startPublicationId);
+        bytes32 endPublicationHash = publicationFeed.getPublicationHash(endCheckpoint.publicationId);
         require(endPublicationHash != 0, "Publication does not exist");
 
         verifier.verifyProof(
-            publicationFeed.getPublicationHash(start.publicationId),
-            endPublicationHash,
-            start.commitment,
-            end.commitment,
-            proof
+            startPublicationHash, endPublicationHash, endPublicationHash, keccak256(endCheckpoint.commitment), proof
         );
 
-        emit TransitionProven(start, end);
+        // TODO: in some cases, this endcheckpoint don't need to be saved, but lets do it in the future after ABI are
+        // finalized.
+        transitions[startChekpointHash] = endCheckpoint;
+        emit TransitionProven(startPublicationId, startCommitmentHash, endCheckpoint);
 
-        bytes32 endHash = keccak256(abi.encode(end));
-        if (startHash == provenHash) {
-            provenHash = endHash;
-            emit CheckpointUpdated(endHash);
-        } else {
-            transitions[startHash] = endHash;
-        }
-
-        _updateProvenHash();
+        _updateLatestCheckpoint();
     }
 
     /// @dev To limit the overhead, we cannot require a proof to find the last proven checkpoint
     /// Instead, each proof should advance the checkpoint by a manageable increment, regardless
     /// of which transition it proves.
-    function _updateProvenHash() internal {
-        bytes32 newProvenHash = transitions[provenHash];
-        if (newProvenHash == 0) {
-            return;
+    function _updateLatestCheckpoint() internal {
+        bytes32 _latestCheckpointHash = latestCheckpointHash;
+        Checkpoint memory _latestCheckpoint;
+
+        for (uint256 i; i < MAX_EXTRA_UPDATES; ++i) {
+            _latestCheckpoint = transitions[_latestCheckpointHash];
+            if (_latestCheckpoint.publicationId == 0) break;
         }
-        // Use another variable to avoid extra storage loads
-        bytes32 nextRecord = newProvenHash;
-        for (uint256 i; i < MAX_EXTRA_UPDATES && nextRecord != 0; ++i) {
-            newProvenHash = nextRecord;
-            nextRecord = transitions[nextRecord];
+
+        if (_latestCheckpointHash != latestCheckpointHash) {
+            latestCheckpointHash = _latestCheckpointHash;
+            latestCheckpoint = _latestCheckpoint;
+            emit CheckpointUpdated(_latestCheckpointHash, _latestCheckpoint);
+
+            // TODO: save latestCheckpoint to signal service?
         }
-        provenHash = newProvenHash;
-        emit CheckpointUpdated(newProvenHash);
     }
 }
--- src/protocol/ICheckpointTracker.sol
+++ src/protocol/ICheckpointTracker.sol
@@ -4,17 +4,19 @@ pragma solidity ^0.8.28;
 interface ICheckpointTracker {
     struct Checkpoint {
         uint256 publicationId;
-        bytes32 commitment;
+        bytes commitment;
     }
 
     /// @notice Emitted when the proven checkpoint is updated
     /// @param checkpointHash the hash of the last proven checkpoint
-    event CheckpointUpdated(bytes32 indexed checkpointHash);
+    /// @param checkpoint the last proven checkpoint
+    event CheckpointUpdated(bytes32 checkpointHash, Checkpoint checkpoint);
 
     /// @notice Emitted when a transition is proven
-    /// @param start the checkpoint before the transition
-    /// @param end the checkpoint after the transition
-    event TransitionProven(Checkpoint start, Checkpoint end);
+    /// @param startPublicationId The ID of the starting publication
+    /// @param startCommitmentHash The hash of the starting commitment
+    /// @param endCheckpoint The final checkpoint after the transition
+    event TransitionProven(uint256 startPublicationId, uint256 startCommitmentHash, Checkpoint endCheckpoint);
 
     // /// @notice Verifies a transition between two checkpoints. Update the latest `provenCheckpoint` if possible
     // /// @param start The initial checkpoint before the transition
