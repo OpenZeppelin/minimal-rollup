b173d4e34135d9032278af9f538e3c7d7f09bbf7
Ernesto García <ernestognw@gmail.com>
2025-02-25 16:30:48 +0800

    Up

--- src/signal/SignalService.sol
+++ src/signal/SignalService.sol
@@ -100,6 +100,7 @@ contract SignalService is ISignalService {
         require(caller == checkpoints(), UnauthorizedCheckpoints(caller));
     }
 
+    /// @dev Allows signaling a `0` value. However, it's considered a no-op for the destination chain.
     function _sendSignal(address account, bytes32 signal) internal virtual returns (bytes32 slot) {
         slot = signalSlot(block.chainid.toUint64(), account, signal);
         slot.getBytes32Slot().value = signal;
