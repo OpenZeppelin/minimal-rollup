b76c422139b55a63ab2bffdf87c86758c505a83d
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2025-02-26 00:20:19 +0800

    expand IProverManager interface

--- src/protocol/IProverManager.sol
+++ src/protocol/IProverManager.sol
@@ -5,6 +5,12 @@ import {ICheckpointTracker} from "./ICheckpointTracker.sol";
 import {IPublicationFeed} from "./IPublicationFeed.sol";
 
 interface IProverManager {
+    function registerProver(uint256 offeredFee) external;
+
+    function exit() external;
+    
+    function evictProver(uint256 publicationId, IPublicationFeed.PublicationHeader calldata publicationHeader) external;
+    
     function proveOwnPeriod(
         ICheckpointTracker.Checkpoint calldata start,
         ICheckpointTracker.Checkpoint calldata end,
