441c708b2b87ccad59af78e0f17e15b64d9ac7a7
Nikesh Nazareth <business@nikeshnazareth.com>
2025-03-20 10:41:30 +1100

    Explain risk of retroactively injecting transactions

--- src/protocol/taiko_alethia/preemptive_assertions/PreemptiveProvableAssertionsBase.sol
+++ src/protocol/taiko_alethia/preemptive_assertions/PreemptiveProvableAssertionsBase.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.28;
 
 /// @notice Allows an L2 proposer to inform L2 contracts about things that will be true at publication time.
 /// Consider the scenario:
-/// - the previous publication was posted to L1 at some time X
-/// - the next publication will be posted to L1 at some future time Z
-/// - the next proposer realises at some intermediate time Y that some claim will be provable on L1 by Z
+/// - the previous publication was posted to L1 at some timestamp X
+/// - the next publication will be posted to L1 at some future timestamp Z
+/// - the next proposer realises at some intermediate real wall-clock time Y that some claim will be provable on L1 by Z
 /// - this typically involves either something that already happened on L1 or something the proposer chooses
 /// Instead of waiting until Z
 /// - the proposer asserts the claim using this L2 contract
@@ -29,6 +29,16 @@ abstract contract PreemptiveProvableAssertionsBase {
     /// @dev The Taiko state-transition function guarantees that the TaikoAnchor.anchor function is called as the first
     /// transaction (in the first L2 block) at the start of every publication.
     /// @dev In practice, this limits access control to the proposer (or builder) that constructs the L1 publication.
+    /// @dev There is a subtlety worth noting. If a proposer uses this modifier to create a preemptive assertion at
+    /// wall-clock time Y, they are effectively retroactively injecting a transaction at the start of the publication
+    /// (immediately after timestamp X), which might invalidate preconfirmations they've already provided. In
+    /// particular, if a transaction (indirectly) accesses `exists` or `value` before time Y, its execution will change
+    /// once the preemptive assertion is included. This should be viewed as an attack. The proposer could either discard
+    /// the transaction (i.e. do not issue a preconfirmation if it accesses a non-existent assertion) or pre-confirm the
+    /// transaction but do not create the invalidating assertion. A more flexible option is to replace this modifier
+    /// with onlyBlockBuilder and use the anchor transaction to set the corresponding address. In this way, preemptive
+    /// assertions would still be limited to the builder but can be made at any time during the publication. However,
+    /// this would be a regular transaction that would charge the builder regular L2 gas.
     modifier onlyAnchor() {
         require(msg.sender == TAIKO_ANCHOR, "Unauthorized");
         _;
