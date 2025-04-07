04a8830ca3e4243aaea1434a0f630beff6c54b86
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2025-04-07 16:09:10 -0300

    tranfer fokens from the initial prover in ERC20ProverManager and expect a CREATE2 deployment

--- src/protocol/ERC20ProverManager.sol
+++ src/protocol/ERC20ProverManager.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.8.28;
 
 import {BaseProverManager} from "./BaseProverManager.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title ERC20ProverManager
 /// @notice Implementation of the `BaseProverManager` contract that uses an ERC20 decided by the deployer of this
 /// contract for bids, stake and paying for publication fees.
+/// @dev This contract expects the `_initialProver` to have already approved this address to spend at least the initial
+/// liveness bond. This amount of tokens will be transferred from the `_initialProver` on the constructor at deployment.
 abstract contract ERC20ProverManager is BaseProverManager {
     using SafeERC20 for IERC20;
 
@@ -18,14 +20,18 @@ abstract contract ERC20ProverManager is BaseProverManager {
         address _publicationFeed,
         address _initialProver,
         uint256 _initialFee,
-        address _token
-    ) payable BaseProverManager(_inbox, _checkpointTracker, _publicationFeed, _initialProver, _initialFee, msg.value) {
+        address _token,
+        uint256 _initialDeposit
+    ) BaseProverManager(_inbox, _checkpointTracker, _publicationFeed, _initialProver, _initialFee, _initialDeposit) {
         require(_token != address(0), "Token address cannot be 0");
+        require(
+            _initialDeposit >= _livenessBond(), "Initial deposit must be greater than or equal to the liveness bond"
+        );
 
         token = IERC20(_token);
 
-        // Deposit the amount of funds needed for the liveness bond from the deployer
-        token.safeTransferFrom(msg.sender, address(this), _livenessBond());
+        // Deposit the amount of funds needed for the liveness bond from the `_initialProver`
+        token.safeTransferFrom(_initialProver, address(this), _initialDeposit);
     }
 
     /// @notice Deposit tokens into the contract. The deposit can be used both for opting in as a prover or proposer
