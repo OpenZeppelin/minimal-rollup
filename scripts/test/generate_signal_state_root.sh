ba1a40d1ac11679e3bc59f12e60b89d1b7b520cf
Leo <leonard.paturel@openzeppelin.com>
2025-04-08 18:02:58 +0100

    make bash script more friendly, fix tests

--- scripts/test/generate_signal_state_root.sh
+++ scripts/test/generate_signal_state_root.sh
@@ -5,6 +5,8 @@
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 CONTRACT_NAME="SignalService"
 ROLLUP_OPERATOR="0xCf03Dd0a894Ef79CB5b601A43C4b25E3Ae4c67eD"
+SIGNAL='0xe321d900f3fd366734e2d071e30949ded20c27fd638f1a059390091c643b62c5'
+SLOT=0xdf4711e2bacf3407f4dd99aff960326754085972cf995e30e8ea995c9080ef00
 BLOCK_TIME=1
 
 # START ANVIL
@@ -18,24 +20,24 @@ forge build -q
 # DEPLOY
 DEPLOY_CMD="forge create --broadcast src/protocol/${CONTRACT_NAME}.sol:${CONTRACT_NAME} --private-key $PRIVATE_KEY --constructor-args $ROLLUP_OPERATOR | grep "
 ADDRESS=$($DEPLOY_CMD | grep "Deployed to:" | awk '{print $3}')
+echo "📦 Contract deployed to: $ADDRESS"
 
 # SEND SIGNAL   
-cast send ${ADDRESS} 'sendSignal(bytes32)' \
-  '0xe321d900f3fd366734e2d071e30949ded20c27fd638f1a059390091c643b62c5' \
+cast send ${ADDRESS} 'sendSignal(bytes32)' ${SIGNAL} \
+   \
   --private-key ${PRIVATE_KEY} --quiet
 
 # GET STATE ROOT
-sleep ${BLOCK_TIME}
 RESPONSE=$(
     curl -s http://localhost:8545 -X POST -H "Content-Type: application/json" \
-        --data '{"method":"eth_getBlockByNumber","params":["0x3",false],"id":1,"jsonrpc":"2.0"}'
+        --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}'
     )
 
 STATE_ROOT=$(echo "$RESPONSE" | jq -r '.result.stateRoot')
 echo "🌳 State root: $STATE_ROOT"
 
 # GET PROOF
-cast proof ${ADDRESS}  0xdf4711e2bacf3407f4dd99aff960326754085972cf995e30e8ea995c9080ef00 | jq
+cast proof ${ADDRESS}  ${SLOT} | jq '{storageHash, accountProof, storageProof}'
 
 
 # KILL ANVIL
--- test/signal/SignalService.t.sol
+++ test/signal/SignalService.t.sol
@@ -16,59 +16,76 @@ contract BaseState is Test {
     MockCheckpointTracker checkpointTracker;
     MockAnchor anchor;
 
+    uint256 public L1Fork;
+    uint256 public L2Fork;
+
     address public rollupOperator = vm.addr(0x1234);
-    address sender = vm.addr(0x2222);
+    address defaultSender = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
     function setUp() public virtual {
-        console.log("msg.sender", msg.sender);
-        vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        L1Fork = vm.createFork("http://localhost:8545");
+        vm.selectFork(L1Fork);
+
+        vm.prank(defaultSender);
         L1signalService = new SignalService(rollupOperator);
-        L2signalService = new SignalService(rollupOperator);
-
-        vm.label(address(L1signalService), "L1signalService");
-        vm.label(address(L2signalService), "L2signalService");
-
         checkpointTracker = new MockCheckpointTracker(address(L1signalService));
-        vm.label(address(checkpointTracker), "CheckpointTracker");
 
+        L2Fork = vm.createFork("http://localhost:8546");
+        vm.selectFork(L2Fork);
+        vm.prank(defaultSender);
+        L2signalService = new SignalService(rollupOperator);
         anchor = new MockAnchor(address(L2signalService));
-        vm.label(address(anchor), "MockAnchor");
+
         vm.prank(rollupOperator);
         L2signalService.setAuthorizedCommitter(address(anchor));
+
+        // Labels for debugging
+        vm.label(address(L1signalService), "L1signalService");
+        vm.label(address(L2signalService), "L2signalService");
+        vm.label(address(checkpointTracker), "CheckpointTracker");
+        vm.label(address(anchor), "MockAnchor");
     }
 }
 
+// State where a signal is sent from L1 to L2.
 contract SendL1SignalState is BaseState {
-    uint256 public value = 0x1234;
+    // 0xe321d900f3fd366734e2d071e30949ded20c27fd638f1a059390091c643b62c5
+    bytes32 public value = keccak256(abi.encode(0x1234));
+    bytes32 public stateRoot = hex"863fb52dc6f4397decb2baaf29af5abf224c675e08ade3623eaf050ac418ee97";
 
     function setUp() public virtual override {
         super.setUp();
-        vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
-        L1signalService.sendSignal(keccak256(abi.encode(value)));
+        vm.prank(defaultSender);
+        L1signalService.sendSignal(value);
+    }
+
+    function getStorageProof() public pure returns (bytes[] memory) {
+        bytes[] memory storageProof = new bytes[](2);
+        storageProof[0] =
+            hex"f85180808080a015ae32d5986f7e597e8a4db519e6640db0eeb8271f30c7691dc58cd99b9cae8d80a0a160b00940ca7a9c5095328a15b5db8f3df8a2fb5044c2a1d68881196bc70e1580808080808080808080";
+        storageProof[1] =
+            hex"f843a0354ada5ea82feea206d357f61e2debe1375f3ba29fb1583a75918c3e84924563a1a0e321d900f3fd366734e2d071e30949ded20c27fd638f1a059390091c643b62c5";
+        return storageProof;
+    }
+
+    function getAccountProof() public pure returns (bytes[] memory) {
+        bytes[] memory accountProof = new bytes[](3);
+        accountProof[0] =
+            hex"f90131a0b91a8b7a7e9d3eab90afd81da3725030742f663c6ed8c26657bf00d842a9f4aaa01689b2a5203afd9ea0a0ca3765e4a538c7176e53eac1f8307a344ffc3c6176558080a04cf95904de6a2222b1265db3c873bc09af47912a53b6f57175fddf2b88a8ffeda099cbe53f7954cea6840ce5238045f5294683729e85deec11239e06e6efe12be38080a04b29efa44ecf50c19b34950cf1d0f05e00568bcc873120fbea9a4e8439de0962a0d0a1bfe5b45d2d863a794f016450a4caca04f3b599e8d1652afca8b752935fd880a0bf9b09e442e044778b354abbadb5ec049d7f5e8b585c3966d476c4fbc9a181d28080a043c8932ac56008879d2f5035d5ee33ba20eeebba97608c62e281dbbbe00d3535a0e5c557a0ce3894afeb44c37f3d24247f67dc76a174d8cacc360c1210eef60a7680";
+        accountProof[1] =
+            hex"f85180808080a00400ea56bf7704ba7dcd741a5d7aaf0967fc83e0483f106d100b416760c7d7fd80808080a074ae0767a40fc6fff780050f46a50f6b39ca4edb7faa9669108157a1cd96f40980808080808080";
+        accountProof[2] =
+            hex"f869a020e659e60b21cc961f64ad47f20523c1d329d4bbda245ef3940a76dc89d0911bb846f8440180a04112cb836908f39193de897177c47c54194a033116b0757b309946d9e456be3ba034dcb5cc12f11a1ef743726b67363c08156d996968139cf2254c04b7a49eadc7";
+        return accountProof;
     }
 }
 
 contract SendL1SignalTest is SendL1SignalState {
     function test_sendL1Signal() public {
-        vm.skip(true);
-        bytes[] memory storageProof = new bytes[](2);
-        storageProof[0] =
-            "0xf85180808080a015ae32d5986f7e597e8a4db519e6640db0eeb8271f30c7691dc58cd99b9cae8d80a0a160b00940ca7a9c5095328a15b5db8f3df8a2fb5044c2a1d68881196bc70e1580808080808080808080";
-        storageProof[1] =
-            "0xf843a0354ada5ea82feea206d357f61e2debe1375f3ba29fb1583a75918c3e84924563a1a0e321d900f3fd366734e2d071e30949ded20c27fd638f1a059390091c643b62c5";
-
-        bytes[] memory accountProof = new bytes[](3);
-        accountProof[0] =
-            "0xf90131a0b91a8b7a7e9d3eab90afd81da3725030742f663c6ed8c26657bf00d842a9f4aaa01689b2a5203afd9ea0a0ca3765e4a538c7176e53eac1f8307a344ffc3c6176558080a0942e72a5b080bd98bb9d56695a657199569dcc880dda8821da899e675cba2c0aa082c69113880d067bc0ec1367caa94b507265709ad98db5915d44b93a6e31cc038080a04b29efa44ecf50c19b34950cf1d0f05e00568bcc873120fbea9a4e8439de0962a0d0a1bfe5b45d2d863a794f016450a4caca04f3b599e8d1652afca8b752935fd880a0bf9b09e442e044778b354abbadb5ec049d7f5e8b585c3966d476c4fbc9a181d28080a0b24ef8583930c8b8dafe2500b23633c2d1bc4d49d4d75f92d6d1bbe53ddfafd7a0e5c557a0ce3894afeb44c37f3d24247f67dc76a174d8cacc360c1210eef60a7680";
-        accountProof[1] =
-            "0xf85180808080a06c4166f9b5f3628a3af12051a3ecfe8a16355d372e20fbcaf6a8e751c418042580808080a074ae0767a40fc6fff780050f46a50f6b39ca4edb7faa9669108157a1cd96f40980808080808080";
-        accountProof[2] =
-            "0xf869a020e659e60b21cc961f64ad47f20523c1d329d4bbda245ef3940a76dc89d0911bb846f8440180a04112cb836908f39193de897177c47c54194a033116b0757b309946d9e456be3ba03aa3f6fb42cd14c9a6c68e36728dfa67f5a6eb85e617e6f97be87baab3ecbb3a";
-
-        ISignalService.SignalProof memory signalProof = ISignalService.SignalProof(accountProof, storageProof);
-        anchor.anchor(1, 0x78d2e23c96c90d06cd025f34fabf1467095ca52fb85077460655c23df846a474);
-        L2signalService.verifySignal(
-            1, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, keccak256(abi.encode(value)), abi.encode(signalProof)
-        );
+        ISignalService.SignalProof memory signalProof = ISignalService.SignalProof(getAccountProof(), getStorageProof());
+        bytes memory encodedProof = abi.encode(signalProof);
+        uint256 height = 1;
+        anchor.anchor(height, stateRoot);
+        L2signalService.verifySignal(height, defaultSender, value, encodedProof);
     }
 }
