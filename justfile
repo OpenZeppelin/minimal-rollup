bc781a154c3385f970178e2a149be76620fc7da3
Leo <leonard.paturel@openzeppelin.com>
2025-04-09 18:20:19 +0100

    use just file fix scripts

--- justfile
+++ justfile
@@ -1,1 +1,26 @@
+# Start two anvil instances
+start-anvil:
+    anvil --port 8545 &
+    anvil --port 8546 &
+    wait
+    echo "🚨WARNING: This starts two background processes🚨"
+    echo "To stop them, run: just stop-anvil"
 
+stop-anvil:
+    lsof -ti:8545 | xargs -r kill
+    lsof -ti:8546 | xargs -r kill
+
+test-int:
+    just start-anvil &
+    wait
+    forge test
+    echo "🚨WARNING: This starts two background processes🚨"
+    echo "To stop them, run: just stop-anvil"
+
+# Get signal proof 
+get-signal-proof signal sender:
+    cargo run --bin signal_proof {{signal}} {{sender}}
+
+# Run the storage slot for a signal and sender
+get-signal-slot signal sender:
+    cargo run --bin signal-slot {{signal}} {{sender}}
---
+++ offchain/signal_proof.rs
@@ -0,0 +1,81 @@
+use alloy::{
+    node_bindings::Anvil,
+    primitives::{address, Address, B256},
+    providers::{Provider, ProviderBuilder},
+    signers::local::PrivateKeySigner,
+    sol,
+};
+use eyre::{eyre, Result};
+use serde_json::to_string_pretty;
+use std::env;
+
+mod signal_slot;
+use signal_slot::get_signal_slot;
+
+const BLOCK_TIME: u64 = 1;
+const ROLLUP_OPERATOR: Address = address!("0xCf03Dd0a894Ef79CB5b601A43C4b25E3Ae4c67eD");
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    SignalService,
+    "./out/SignalService.sol/SignalService.json",
+);
+
+fn prompt_sender_and_signal() -> Result<(B256, Address)> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        return Err(eyre!(
+            "Usage: cargo run --bin signal_proof <signal (0x...)> <sender (0x...)>"
+        ));
+    }
+
+    let signal_str = &args[1];
+    let signal: B256 = signal_str
+        .parse()
+        .map_err(|_| eyre!("Invalid signal format: {}", signal_str))?;
+
+    let sender_str = &args[2];
+    let sender: Address = sender_str
+        .parse()
+        .map_err(|_| eyre!("Invalid sender format: {}", sender_str))?;
+
+    Ok((signal, sender))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!();
+    let (signal, sender) = prompt_sender_and_signal()?;
+
+    let anvil = Anvil::new().block_time(BLOCK_TIME).try_spawn()?;
+    let signer: PrivateKeySigner = anvil.keys()[0].clone().into();
+
+    let slot = get_signal_slot(&signal, &sender);
+
+    let rpc_url = anvil.endpoint_url();
+    let provider = ProviderBuilder::new().wallet(signer).on_http(rpc_url);
+
+    let contract = SignalService::deploy(&provider, ROLLUP_OPERATOR).await?;
+
+    println!("Deployed contract at address: {}", contract.address());
+
+    let builder = contract.sendSignal(signal);
+    builder.send().await?.watch().await?;
+
+    let state_root = provider
+        .get_block_by_number(alloy::eips::BlockNumberOrTag::Latest)
+        .await?
+        .unwrap()
+        .header
+        .state_root;
+
+    let proof = provider
+        .get_proof(contract.address().to_owned(), vec![slot])
+        .await?;
+
+    println!("State Root: {:?}", state_root);
+    println!("Account Proof: {}", to_string_pretty(&proof.account_proof)?);
+    println!("Storage Proof: {}", to_string_pretty(&proof.storage_proof)?);
+    Ok(())
+}
