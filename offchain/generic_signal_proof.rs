babc4f017ed8ba15549610f4eb952d4c8535ff00
Leo <leonard.paturel@openzeppelin.com>
2025-04-17 15:29:49 +0100

    fix rust scripts

--- offchain/generic_signal_proof.rs
+++ offchain/generic_signal_proof.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use alloy::primitives::{Address, B256};
+use alloy::primitives::{Address, B256, U256};
 use eyre::{eyre, Result};
 
 mod signal_slot;
@@ -9,9 +9,9 @@ use signal_slot::{get_signal_slot, NameSpaceConst};
 mod utils;
 use utils::{deploy_signal_service, get_proofs, get_provider};
 
-pub fn prompt_sender_and_signal() -> Result<(B256, Address)> {
+pub fn prompt_sender_and_signal() -> Result<(B256, U256, Address)> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 4 {
         return Err(eyre!(
             "Must provide both the signal/value and sender address"
         ));
@@ -27,13 +27,18 @@ pub fn prompt_sender_and_signal() -> Result<(B256, Address)> {
         .parse()
         .map_err(|_| eyre!("Invalid sender format: {}", sender_str))?;
 
-    Ok((signal, sender))
+    let chain_id_str = &args[3];
+    let chain_id: U256 = chain_id_str
+        .parse()
+        .map_err(|_| eyre!("Invalid chain id: {}", chain_id_str))?;
+
+    Ok((signal, chain_id, sender))
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!();
-    let (signal, sender) = prompt_sender_and_signal()?;
+    let (signal, chain_id, sender) = prompt_sender_and_signal()?;
 
     let (provider, _anvil) = get_provider()?;
 
@@ -48,7 +53,7 @@ async fn main() -> Result<()> {
     let builder = signal_service.sendSignal(signal);
     builder.send().await?.watch().await?;
 
-    let slot = get_signal_slot(&signal, &sender, NameSpaceConst::Signal);
+    let slot = get_signal_slot(&signal, &sender, &chain_id, NameSpaceConst::Signal);
     get_proofs(&provider, slot, &signal_service).await?;
 
     Ok(())
