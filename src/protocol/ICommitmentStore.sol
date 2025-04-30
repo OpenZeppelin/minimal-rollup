807fa568a1b226ccfde391f67a7c43e8077a5734
Leo <leonard.paturel@openzeppelin.com>
2025-04-30 11:34:03 +0200

    remove authorize in the commitment store
    height --> index

--- src/protocol/ICommitmentStore.sol
+++ src/protocol/ICommitmentStore.sol
@@ -6,36 +6,22 @@ import {ICheckpointTracker} from "./ICheckpointTracker.sol";
 /// @dev Stores commitments from different chains.
 ///
 /// A commitment is any value (typically a state root) that uniquely identifies the state of a chain at a
-/// specific height (i.e. an incremental identifier like a blockNumber, publicationId or even a timestamp).
-/// Only an authorized committer can store commitments. For example, only the `CheckpointTracker` can store roots on the
-/// L1,
-/// and the anchor can store block hashes on the L2.
+/// specific index (i.e. an incremental identifier like a blockNumber, publicationId or even a timestamp).
+///
+/// There is no access control so only commitments from trusted sources should be used.
+/// For example, L2 contracts should use L1 commitments saved by the anchor contract and L1 contracts should use L2
+/// commitments saved by the relevant `CheckpointTracker`.
 interface ICommitmentStore {
-    /// @dev A new `commitment` has been stored at a specified `height`.
-    event CommitmentStored(uint256 indexed height, bytes32 commitment);
+    /// @dev A new `commitment` has been stored by `source` at a specified `index`.
+    event CommitmentStored(address indexed source, uint256 indexed index, bytes32 commitment);
 
-    /// @dev Emitted when the authorized committer is updated.
-    event AuthorizedCommitterUpdated(address newAuthorizedCommitter);
-
-    /// @dev The caller is not a recognized authorized committer.
-    error UnauthorizedCommitter();
-
-    /// @dev The trusted committer address is empty.
-    error EmptyCommitter();
-
-    /// @dev Returns the current authorized committer.
-    function authorizedCommitter() external view returns (address);
-
-    /// @dev Sets a new authorized committer.
-    /// @param newAuthorizedCommitter The new authorized committer address
-    function setAuthorizedCommitter(address newAuthorizedCommitter) external;
-
-    /// @dev Returns the commitment at the given `height`.
-    /// @param height The height of the commitment
-    function commitmentAt(uint256 height) external view returns (bytes32 commitment);
+    /// @dev Returns the commitment at the given `index`.
+    /// @param source The source address for the saved commitment
+    /// @param index The index of the commitment
+    function commitmentAt(address source, uint256 index) external view returns (bytes32 commitment);
 
     /// @dev Stores a commitment.
-    /// @param height The height of the commitment
+    /// @param index The index of the commitment
     /// @param commitment The commitment to store
-    function storeCommitment(uint256 height, bytes32 commitment) external;
+    function storeCommitment(uint256 index, bytes32 commitment) external;
 }
