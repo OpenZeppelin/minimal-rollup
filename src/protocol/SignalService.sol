717b414ddac35cbf1cc253a8bd05b37a07731e61
Leo <leonard.paturel@openzeppelin.com>
2025-04-30 11:39:44 +0200

    update to get commitment from a 'trusted' source

--- src/protocol/SignalService.sol
+++ src/protocol/SignalService.sol
@@ -10,15 +10,12 @@ import {ISignalService} from "./ISignalService.sol";
 ///
 /// This contract allows sending arbitrary data as signals via `sendSignal`, verifying signals from other chains using
 /// `verifySignal`, and bridging native ETH with built-in signal generation and verification. It integrates:
-/// - `CommitmentStore` to access trusted state roots,
-/// - `ETHBridge` for native ETH bridging with deposit and claim flows,
+/// - `CommitmentStore` to access state roots,
 /// - `LibSignal` for signal hashing, storage, and verification logic.
 ///
 /// Signals stored cannot be deleted and can be verified multiple times.
-contract SignalService is ISignalService, ETHBridge, CommitmentStore {
+contract SignalService is ISignalService, CommitmentStore {
     using LibSignal for bytes32;
-
-    constructor(address _rollupOperator) CommitmentStore(_rollupOperator) {}
 
     /// @inheritdoc ISignalService
     /// @dev Signals are stored in a namespaced slot derived from the signal value, sender address and SIGNAL_NAMESPACE
@@ -36,8 +33,10 @@ contract SignalService is ISignalService, ETHBridge, CommitmentStore {
 
     /// @inheritdoc ISignalService
     /// @dev Cannot be used to verify signals that are under the eth-bridge namespace.
-    function verifySignal(uint256 height, address sender, bytes32 value, bytes memory proof) external {
-        _verifySignal(height, sender, value, LibSignal.SIGNAL_NAMESPACE, proof);
+    function verifySignal(uint256 index, address commitmentPublisher, address sender, bytes32 value, bytes memory proof)
+        external
+    {
+        _verifySignal(index, commitmentPublisher, sender, value, LibSignal.SIGNAL_NAMESPACE, proof);
         emit SignalVerified(sender, value);
     }
 
@@ -50,27 +49,30 @@ contract SignalService is ISignalService, ETHBridge, CommitmentStore {
     // CHECK: Should this function be non-reentrant?
     /// @inheritdoc ETHBridge
     /// @dev Overrides ETHBridge.claimDeposit to add signal verification logic.
-    function claimDeposit(ETHDeposit memory ethDeposit, uint256 height, bytes memory proof)
+    function claimDeposit(ETHDeposit memory ethDeposit, uint256 index, bytes memory proof)
         external
         override
         returns (bytes32 id)
     {
         id = _generateId(ethDeposit);
 
-        _verifySignal(height, ethDeposit.from, id, ETH_BRIDGE_NAMESPACE, proof);
+        _verifySignal(index, commitmentPublisher, ethDeposit.from, id, ETH_BRIDGE_NAMESPACE, proof);
 
         super._processClaimDepositWithId(id, ethDeposit);
     }
 
-    function _verifySignal(uint256 height, address sender, bytes32 value, bytes32 namespace, bytes memory proof)
-        internal
-        view
-        virtual
-    {
+    function _verifySignal(
+        uint256 index,
+        address commitmentPublisher,
+        address sender,
+        bytes32 value,
+        bytes32 namespace,
+        bytes memory proof
+    ) internal view virtual {
         // TODO: commitmentAt(height) might not be the 'state root' of the chain
         // For now it could be the block hash or other hashed value
         // further work is needed to ensure we get the 'state root' of the chain
-        bytes32 root = commitmentAt(height);
+        bytes32 root = commitmentAt(commitmentPublisher, index);
         SignalProof memory signalProof = abi.decode(proof, (SignalProof));
         bytes[] memory accountProof = signalProof.accountProof;
         bytes[] memory storageProof = signalProof.storageProof;
