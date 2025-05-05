2608edb5d52fe14d07a2fdf69a6ee96f90aded10
Nikesh Nazareth <business@nikeshnazareth.com>
2025-05-05 14:45:13 +0200

    Remove unnecessary import

--- src/protocol/CommitmentStore.sol
+++ src/protocol/CommitmentStore.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {ICheckpointTracker} from "./ICheckpointTracker.sol";
 import {ICommitmentStore} from "./ICommitmentStore.sol";
 
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
