d9340f6364679e0715ba7549a0ad53b5bb4e13ac
Leo <leonard.paturel@openzeppelin.com>
2025-05-06 09:50:32 +0200

    fix comment about state proof in lib signal

--- src/libs/LibSignal.sol
+++ src/libs/LibSignal.sol
@@ -48,9 +48,8 @@ library LibSignal {
     /// @dev Performs a storage proof verification for a signal stored on the contract using this library
     /// @param value The signal value to verify
     /// @param sender The address that originally sent the signal on the source chain
-    /// @param root The state root or storage root from the source chain to verify against
-    /// @param accountProof Merkle proof for the SignalService account against the state root. Empty if we are using a
-    /// storage root.
+    /// @param root The state root from the source chain to verify against
+    /// @param accountProof Merkle proof for the SignalService account against the state root.
     /// @param storageProof Merkle proof for the derived storage slot against the SignalService's storage root
     /// @dev We can use `address(this)` as the SignalService address, even when `root` refers to a different chain,
     /// because we assume the SignalService is deployed at the same address on every chain.
