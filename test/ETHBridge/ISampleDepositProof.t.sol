5397b0b181a38599a3266504365d9d8cea6e0928
Nikesh Nazareth <business@nikeshnazareth.com>
2025-05-15 19:22:40 +1000

    Use storage to save sample deposits.
    This is in preparation for storing multiple proofs in the contract

--- test/ETHBridge/ISampleDepositProof.t.sol
+++ test/ETHBridge/ISampleDepositProof.t.sol
@@ -9,6 +9,6 @@ import {ISignalService} from "src/protocol/ISignalService.sol";
 interface ISampleDepositProof {
     function getSourceAddresses() external returns (address signalService, address bridge);
     function getDepositSignalProof() external returns (ISignalService.SignalProof memory);
-    function getEthDeposit() external pure returns (IETHBridge.ETHDeposit memory deposit);
-    function getDepositInternals() external pure returns (bytes32 slot, bytes32 id);
+    function getEthDeposit() external returns (IETHBridge.ETHDeposit memory deposit);
+    function getDepositInternals() external returns (bytes32 slot, bytes32 id);
 }
--- test/ETHBridge/SampleDepositProof.t.sol
+++ test/ETHBridge/SampleDepositProof.t.sol
@@ -5,11 +5,47 @@ pragma solidity ^0.8.28;
 // Do not edit manually.
 // See ISampleDepositProof.t.sol for an explanation of its purpose.
 
-import {ISampleDepositProof} from "./ISampleDepositProof.t.sol";
-import {IETHBridge} from "src/protocol/IETHBridge.sol";
 import {ISignalService} from "src/protocol/ISignalService.sol";
+import {IETHBridge} from "src/protocol/IETHBridge.sol";
+import {ISampleDepositProof} from "./ISampleDepositProof.t.sol";
 
 contract SampleDepositProof is ISampleDepositProof {
+
+    ISignalService.SignalProof[] private signalProofs;
+    IETHBridge.ETHDeposit[] private deposits;
+    bytes32[] private slots;
+    bytes32[] private ids;
+
+    constructor() {
+        ISignalService.SignalProof memory signalProof = ISignalService.SignalProof({
+            accountProof: new bytes[](3),
+            storageProof: new bytes[](1),
+            stateRoot: bytes32(0xb7711f031190aae5617914df7c3d4d2d2496031986f5b8dcd6ac7d8daeef424b),
+            blockHash: bytes32(0x7e33c4721cdeeb0a3dafce19a154219f09700c3d4f1a7457e751a436d6b80d68)
+        });
+        
+		signalProof.accountProof[0] = hex"f90151a0b91a8b7a7e9d3eab90afd81da3725030742f663c6ed8c26657bf00d842a9f4aaa01689b2a5203afd9ea0a0ca3765e4a538c7176e53eac1f8307a344ffc3c6176558080a003c2f506dcc4351bbb1f7943084665e21af2cad3f37618a794167901174fc2caa07b9b5af76aaacfe822062ed002c6db0b494ae66b9dbe3e91c7c398088a92090480a0931c5d4ef4b2277973028722fd557c9eb65b9db5f7788c27005ce5ae5c677a86a04b29efa44ecf50c19b34950cf1d0f05e00568bcc873120fbea9a4e8439de0962a0d0a1bfe5b45d2d863a794f016450a4caca04f3b599e8d1652afca8b752935fd880a0bf9b09e442e044778b354abbadb5ec049d7f5e8b585c3966d476c4fbc9a181d28080a046e3a6bd785b2f60ecc9f58b5302daf708d8328307670dba4b5226236db6f148a0e5c557a0ce3894afeb44c37f3d24247f67dc76a174d8cacc360c1210eef60a7680";
+		signalProof.accountProof[1] = hex"f85180808080a01de1625cbf687e609d7804db12584ef214fabb3b641724a737b2f3fc8496957480808080a074ae0767a40fc6fff780050f46a50f6b39ca4edb7faa9669108157a1cd96f40980808080808080";
+		signalProof.accountProof[2] = hex"f869a020e659e60b21cc961f64ad47f20523c1d329d4bbda245ef3940a76dc89d0911bb846f8440180a095aad4770cd67977ac25c687f94d3ccaaeb0ae1bf39482feba4717bc2b1b888fa0da147a8683b303efc72285d333a0683c61d218e18e8dbc84e4cbf5885d4a9229";
+        
+		signalProof.storageProof[0] = hex"e3a120a4fae02acd15bbf46ecee8fa4a72c97d17c1e2e719ac7517b63a1c1cde135a1801";
+
+        signalProofs.push(signalProof);
+
+        deposits.push(
+            IETHBridge.ETHDeposit({
+                nonce: 0,
+                from: address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266),
+                to: address(0x99A270Be1AA5E97633177041859aEEB9a0670fAa),
+                amount: 4000000000000000000,
+                data: bytes("")
+            })
+        );
+
+        slots.push(bytes32(0xc0861da4adda5f350f55acc981f72fcac9ffc3c97d3b55c7525f254986065300));
+        ids.push(bytes32(0x103b147e6dacf071f9fe23d0088a650be67e2412f334115cbc3fa20404b360fd));
+    }
+
     /// @inheritdoc ISampleDepositProof
     function getSourceAddresses() public pure returns (address signalService, address bridge) {
         signalService = address(0x5FbDB2315678afecb367f032d93F642f64180aa3);
@@ -17,38 +53,17 @@ contract SampleDepositProof is ISampleDepositProof {
     }
 
     /// @inheritdoc ISampleDepositProof
-    function getDepositSignalProof() public pure returns (ISignalService.SignalProof memory signalProof) {
-        signalProof = ISignalService.SignalProof({
-            accountProof: new bytes[](3),
-            storageProof: new bytes[](1),
-            stateRoot: bytes32(0xb7711f031190aae5617914df7c3d4d2d2496031986f5b8dcd6ac7d8daeef424b),
-            blockHash: bytes32(0x84754b3a7aaed16f1f801ee0461ad546b6c338c77c8481ae7392a0e40ce157d4)
-        });
-
-        signalProof.accountProof[0] =
-            hex"f90151a0b91a8b7a7e9d3eab90afd81da3725030742f663c6ed8c26657bf00d842a9f4aaa01689b2a5203afd9ea0a0ca3765e4a538c7176e53eac1f8307a344ffc3c6176558080a003c2f506dcc4351bbb1f7943084665e21af2cad3f37618a794167901174fc2caa07b9b5af76aaacfe822062ed002c6db0b494ae66b9dbe3e91c7c398088a92090480a0931c5d4ef4b2277973028722fd557c9eb65b9db5f7788c27005ce5ae5c677a86a04b29efa44ecf50c19b34950cf1d0f05e00568bcc873120fbea9a4e8439de0962a0d0a1bfe5b45d2d863a794f016450a4caca04f3b599e8d1652afca8b752935fd880a0bf9b09e442e044778b354abbadb5ec049d7f5e8b585c3966d476c4fbc9a181d28080a046e3a6bd785b2f60ecc9f58b5302daf708d8328307670dba4b5226236db6f148a0e5c557a0ce3894afeb44c37f3d24247f67dc76a174d8cacc360c1210eef60a7680";
-        signalProof.accountProof[1] =
-            hex"f85180808080a01de1625cbf687e609d7804db12584ef214fabb3b641724a737b2f3fc8496957480808080a074ae0767a40fc6fff780050f46a50f6b39ca4edb7faa9669108157a1cd96f40980808080808080";
-        signalProof.accountProof[2] =
-            hex"f869a020e659e60b21cc961f64ad47f20523c1d329d4bbda245ef3940a76dc89d0911bb846f8440180a095aad4770cd67977ac25c687f94d3ccaaeb0ae1bf39482feba4717bc2b1b888fa0da147a8683b303efc72285d333a0683c61d218e18e8dbc84e4cbf5885d4a9229";
-
-        signalProof.storageProof[0] = hex"e3a120a4fae02acd15bbf46ecee8fa4a72c97d17c1e2e719ac7517b63a1c1cde135a1801";
+    function getDepositSignalProof() public view returns (ISignalService.SignalProof memory signalProof) {
+        return signalProofs[0];
     }
 
     /// @inheritdoc ISampleDepositProof
-    function getEthDeposit() public pure returns (IETHBridge.ETHDeposit memory deposit) {
-        deposit = IETHBridge.ETHDeposit({
-            nonce: 0,
-            from: address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266),
-            to: address(0x99A270Be1AA5E97633177041859aEEB9a0670fAa),
-            amount: 4000000000000000000,
-            data: bytes("")
-        });
+    function getEthDeposit() public view returns (IETHBridge.ETHDeposit memory deposit) {
+        return deposits[0];
     }
 
     /// @inheritdoc ISampleDepositProof
-    function getDepositInternals() public pure returns (bytes32 slot, bytes32 id) {
-        slot = bytes32(0xc0861da4adda5f350f55acc981f72fcac9ffc3c97d3b55c7525f254986065300);
-        id = bytes32(0x103b147e6dacf071f9fe23d0088a650be67e2412f334115cbc3fa20404b360fd);
+    function getDepositInternals() public view returns (bytes32 slot, bytes32 id) {
+        return (slots[0], ids[0]);
     }
 }
