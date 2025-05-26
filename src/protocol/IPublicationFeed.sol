d9b7029ad9fb95076d235d6e44ad46610c70fa13
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2025-05-26 15:47:07 -0400

    merge publication feed and inbox

--- src/protocol/IPublicationFeed.sol
+++ src/protocol/IPublicationFeed.sol
@@ -17,13 +17,6 @@ interface IPublicationFeed {
     /// @param attributes The data contained within the publication
     event Published(bytes32 indexed pubHash, PublicationHeader header, bytes[] attributes);
 
-    /// @notice Publish arbitrary data into the global queue
-    /// @param attributes The data to publish
-    /// @return header The publication header
-    /// @dev The data is encoded as an array so each attribute is hashed independently,
-    /// which allows a single attribute to be validated against a pubHash
-    function publish(bytes[] calldata attributes) external returns (PublicationHeader memory header);
-
     /// @notice Retrieve a hash representing a previous publication
     /// @param idx The index of the publication hash
     /// @return _ The corresponding publication hash
--- src/protocol/taiko_alethia/TaikoInbox.sol
+++ src/protocol/taiko_alethia/TaikoInbox.sol
@@ -11,46 +11,48 @@ import {IInbox} from "../IInbox.sol";
 import {ILookahead} from "../ILookahead.sol";
 import {IProposerFees} from "../IProposerFees.sol";
 
-contract TaikoInbox is IInbox, DelayedInclusionStore {
+contract TaikoInbox is IInbox, IPublicationFeed, DelayedInclusionStore {
     struct Metadata {
         uint256 anchorBlockId;
         bytes32 anchorBlockHash;
         bool isDelayedInclusion;
     }
 
-    IPublicationFeed public immutable publicationFeed;
     ILookahead public immutable lookahead;
     IProposerFees public immutable proposerFees;
-
     uint256 public immutable maxAnchorBlockIdOffset;
-
-    uint64 public lastPublicationId;
 
     // attributes associated with the publication
     uint256 private constant METADATA = 0;
     uint256 private constant LAST_PUBLICATION = 1;
     uint256 private constant BLOB_REFERENCE = 2;
 
+    bytes32[] private _publicationHashes;
+
     constructor(
-        address _publicationFeed,
         address _lookahead,
         address _blobRefRegistry,
         uint256 _maxAnchorBlockIdOffset,
         address _proposerFees,
         uint256 _inclusionDelay
     ) DelayedInclusionStore(_inclusionDelay, _blobRefRegistry) {
-        publicationFeed = IPublicationFeed(_publicationFeed);
+        require(_proposerFees != address(0), "Invalid proposer fees address");
+
         lookahead = ILookahead(_lookahead);
         maxAnchorBlockIdOffset = _maxAnchorBlockIdOffset;
         proposerFees = IProposerFees(_proposerFees);
+
+        // guarantee there is always a previous hash
+        _publicationHashes.push(0);
     }
 
+    /// @inheritdoc IInbox
     function publish(uint256 nBlobs, uint64 anchorBlockId) external payable {
         if (address(lookahead) != address(0)) {
             require(lookahead.isCurrentPreconfer(msg.sender), "not current preconfer");
         }
 
-        uint256 _lastPublicationId = lastPublicationId;
+        uint256 _lastPublicationId = _publicationHashes.length - 1;
 
         // Build the attribute for the anchor transaction inputs
         require(anchorBlockId >= block.number - maxAnchorBlockIdOffset, "anchorBlockId too old");
@@ -68,7 +70,7 @@ contract TaikoInbox is IInbox, DelayedInclusionStore {
         attributes[BLOB_REFERENCE] = abi.encode(blobRefRegistry.getRef(_buildBlobIndices(nBlobs)));
 
         proposerFees.payPublicationFee(msg.sender, false);
-        _lastPublicationId = publicationFeed.publish(attributes).id;
+        _lastPublicationId = _publish(attributes).id;
 
         // Publish each delayed inclusion as a separate publication
         IDelayedInclusionStore.Inclusion[] memory inclusions = processDueInclusions();
@@ -81,12 +83,54 @@ contract TaikoInbox is IInbox, DelayedInclusionStore {
             attributes[BLOB_REFERENCE] = abi.encode(inclusions[i]);
 
             proposerFees.payPublicationFee(msg.sender, true);
-            _lastPublicationId = publicationFeed.publish(attributes).id;
+            _lastPublicationId = _publish(attributes).id;
         }
-
-        lastPublicationId = uint64(_lastPublicationId);
     }
 
+    /// @dev Internal implementation of publication logic
+    /// @param attributes The data to publish
+    /// @return header The publication header
+    function _publish(bytes[] memory attributes) internal returns (PublicationHeader memory header) {
+        uint256 nAttributes = attributes.length;
+        bytes32[] memory attributeHashes = new bytes32[](nAttributes);
+        for (uint256 i; i < nAttributes; ++i) {
+            attributeHashes[i] = keccak256(attributes[i]);
+        }
+
+        uint256 id = _publicationHashes.length;
+        header = PublicationHeader({
+            id: id,
+            prevHash: _publicationHashes[id - 1],
+            publisher: msg.sender,
+            timestamp: block.timestamp,
+            blockNumber: block.number,
+            attributesHash: keccak256(abi.encode(attributeHashes))
+        });
+
+        bytes32 pubHash = keccak256(abi.encode(header));
+        _publicationHashes.push(pubHash);
+
+        emit Published(pubHash, header, attributes);
+    }
+
+    /// @inheritdoc IPublicationFeed
+    function getPublicationHash(uint256 idx) external view returns (bytes32) {
+        return _publicationHashes[idx];
+    }
+
+    /// @inheritdoc IPublicationFeed
+    function getNextPublicationId() external view returns (uint256) {
+        return _publicationHashes.length;
+    }
+
+    /// @inheritdoc IPublicationFeed
+    function validateHeader(PublicationHeader calldata header) external view returns (bool) {
+        return keccak256(abi.encode(header)) == _publicationHashes[header.id];
+    }
+
+    /// @dev Builds blob indices array for the given number of blobs
+    /// @param nBlobs Number of blobs to create indices for
+    /// @return blobIndices Array of blob indices
     function _buildBlobIndices(uint256 nBlobs) private pure returns (uint256[] memory blobIndices) {
         blobIndices = new uint256[](nBlobs);
         for (uint256 i; i < nBlobs; ++i) {
