039b488f1579b83b668c990e5b374870c4d63cb4
Nikesh Nazareth <business@nikeshnazareth.com>
2025-06-11 21:56:16 +1000

    Simplify MockInbox and MockCheckpoint
    They now revert on every function call. I will expand the functionality as needed

--- test/mocks/MockCheckpointTracker.sol
+++ test/mocks/MockCheckpointTracker.sol
@@ -2,44 +2,19 @@
 pragma solidity ^0.8.28;
 
 import {ICheckpointTracker} from "src/protocol/ICheckpointTracker.sol";
-import {ICommitmentStore} from "src/protocol/ICommitmentStore.sol";
 
 contract MockCheckpointTracker is ICheckpointTracker {
-    Checkpoint private provenCheckpoint;
-    ICommitmentStore public immutable commitmentStore;
+    error NotImplemented();
 
-    constructor(address _commitmentStore) {
-        // Set the initial proven hash to the zero checkpoint
-        Checkpoint memory zeroCheckpoint = Checkpoint({publicationId: 0, commitment: bytes32(0)});
-        provenCheckpoint = zeroCheckpoint;
-        commitmentStore = ICommitmentStore(_commitmentStore);
+    function getProvenCheckpoint() external pure returns (Checkpoint memory) {
+        revert NotImplemented();
     }
 
-    /// @notice Do nothing. All checkpoints and proofs are accepted.
-    function proveTransition(
-        Checkpoint calldata start,
-        Checkpoint calldata end,
-        uint256 numDelayedPublications,
-        bytes calldata
-    ) external returns (uint256) {
-        commitmentStore.storeCommitment(end.publicationId, end.commitment);
-
-        // Just return the total number of publications passed
-        uint256 numPublications = end.publicationId - start.publicationId;
-        require(
-            numDelayedPublications <= numPublications,
-            "Number of delayed publications cannot be greater than the total number of publications"
-        );
-        return end.publicationId - provenCheckpoint.publicationId;
-    }
-
-    /// @notice Helper to set the proven hash for easier testing
-    /// @param checkpoint the checkpoint to set as proven, that will be hashed and stored as the proven hash
-    function setProvenHash(Checkpoint memory checkpoint) external {
-        provenCheckpoint = checkpoint;
-    }
-
-    function getProvenCheckpoint() external view returns (Checkpoint memory) {
-        return provenCheckpoint;
+    function proveTransition(Checkpoint calldata, Checkpoint calldata, uint256, bytes calldata)
+        external
+        pure
+        returns (uint256)
+    {
+        revert NotImplemented();
     }
 }
