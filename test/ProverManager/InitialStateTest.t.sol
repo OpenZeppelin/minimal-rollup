2cd50fb299db9b797e9e4c22ff99a59be6ac54e6
Nikesh Nazareth <business@nikeshnazareth.com>
2025-06-16 20:16:44 +1000

    Test initial state periods

--- test/ProverManager/InitialStateTest.t.sol
+++ test/ProverManager/InitialStateTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 import {ERC20Currency, ETHCurrency} from "./CurrencyScenario.t.sol";
 import {InitialState} from "./InitialState.t.sol";
+import {LibProvingPeriod} from "src/libs/LibProvingPeriod.sol";
 
 /// Represents the initial state of the ProverManager contract after deployment.
 /// @dev This should be inherited to cover the ETH and ERC20 scenarios.
@@ -18,6 +19,37 @@ abstract contract InitialStateTest is InitialState {
 
     // Confirm the ProverManager address retrieved the initial deposit
     function test_ProverManagerHasInitialDeposit() public view virtual;
+
+    function test_CurrentPeriodIsZero() public view {
+        // The current period does not change until there is a proposal in the new period
+        assertEq(proverManager.currentPeriodId(), 0, "Current period is not 0");
+    }
+
+    function test_PeriodZeroIsEmptyAndEndsThisBlock() public view {
+        LibProvingPeriod.Period memory period = proverManager.getPeriod(0);
+        assertEq(period.prover, address(0), "Period 0 prover is not address(0)");
+        assertEq(period.stake, 0, "Period 0 stake is not 0");
+        assertEq(period.fee, 0, "Period 0 fee is not 0");
+        assertEq(period.delayedFeePercentage, 0, "Period 0 delayed fee percentage is not 0");
+        assertEq(period.end, vm.getBlockTimestamp(), "Period 0 end is not this block timestamp");
+        assertEq(period.deadline, vm.getBlockTimestamp(), "Period 0 deadline is not this block timestamp");
+        assertEq(period.pastDeadline, false, "Period 0 has missed deadline");
+    }
+
+    function test_PeriodOneIsInitialized() public view {
+        LibProvingPeriod.Period memory period = proverManager.getPeriod(1);
+        assertEq(period.prover, initialProver, "Period 1 prover is not initialProver");
+        assertEq(period.stake, proverManager.livenessBond(), "Period 1 stake is not liveness bond");
+        assertEq(period.fee, initialFee, "Period 1 fee is not initialFee");
+        assertEq(
+            period.delayedFeePercentage,
+            proverManager.delayedFeePercentage(),
+            "Period 1 delayed fee percentage does not match prover manager config"
+        );
+        assertEq(period.end, 0, "Period 1 has end timestamp");
+        assertEq(period.deadline, 0, "Period 1 has deadline");
+        assertEq(period.pastDeadline, false, "Period 0 has missed deadline");
+    }
 }
 
 contract InitialStateTest_ETH is InitialStateTest, ETHCurrency {
