a0fed5246729f9e52d508734134d4595c63a9ab8
Nikesh Nazareth <business@nikeshnazareth.com>
2025-08-06 13:41:40 +1000

    Migrate RelayRecipientScenarios to new structure

--- test/MessageRelayer/RelayRecipientScenarios.t.sol
+++ test/MessageRelayer/RelayRecipientScenarios.t.sol
@@ -1,22 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {DepositRecipientIsMessageRelayer} from "./DepositRecipientScenarios.t.sol";
+import {NoGasLimit_SufficientGasProvided} from "./GasLimitScenarios.t.sol";
 import {GenericRecipient} from "./GenericRecipient.t.sol";
 
 import {InitialState} from "./InitialState.t.sol";
 import {IETHBridge} from "src/protocol/IETHBridge.sol";
 
-contract RelayRecipientScenarios is DepositRecipientIsMessageRelayer {
-    function test_RelayRecipientRejectsDeposit_relayMessage_shouldRevert() public {
-        to.setSuccess(false);
-        vm.expectRevert(IETHBridge.FailedClaim.selector);
-        _relayMessage();
-    }
+// Use NoGasLimit_SufficientGasProvided as the default scenario.
+// Any valid tip arrangement, funding amount and gas limit should suffice for these tests.
+abstract contract RelayRecipientScenarios is NoGasLimit_SufficientGasProvided {}
 
-    function test_RelayRecipientReentersReceiveMessage_relayMessage_shouldRevert() public {
+contract RelayRecipentAcceptsMessage is RelayRecipientScenarios {}
+
+contract RelayRecipientRejectsMessage is RelayRecipientScenarios {
+    function setUp() public override {
+        super.setUp();
+        to.setSuccess(false);
+        relayShouldSucceed = false;
+        claimShouldSucceed = false;
+    }
+}
+
+contract RelayRecipientReentersReceiveMessage is RelayRecipientScenarios {
+    function setUp() public override {
+        super.setUp();
         to.setReentrancyAttack(true);
-        vm.expectRevert(IETHBridge.FailedClaim.selector);
-        _relayMessage();
+        relayShouldSucceed = false;
+        claimShouldSucceed = false;
     }
 }
